9f81f815ffe8c32e7a7a60a928055a4dd8e00a83
TheIncgi <theincgi@gmail.com>
2024-04-01 21:03:31 -0400

    remove delta from int check
    oops

--- src/test/java/gplua/cl/LuaTests.java
+++ src/test/java/gplua/cl/LuaTests.java
@@ -157,7 +157,7 @@ public class LuaTests extends KernelTestBase {
 		
 		assertEquals(1, results.length);
 		assertEquals(LuaTypes.INT, results[0].type());
-		assertEquals(-9, results[0].intValue(), .0000001);
+		assertEquals(-9, results[0].intValue());
 	}
 	
 }
